ae11ff39b632bfd42382419a50c64afb93c73409
manojsuthar-capiter <manoj.suthar@capiter.net>
2022-09-21 19:06:39 +0530

    Initial commit

---
+++ src/main/java/com/spring/websocket/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.spring.websocket.controller;
+
+import model.Greeting;
+import model.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class HomeController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
